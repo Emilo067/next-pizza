09108d311b0ead017c6299890fee803e426b8cc1
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 09:22:35 +0300

    Initial App With Header

---
+++ components/shared/container.tsx
@@ -0,0 +1,11 @@
+import {cn} from "@/lib/utils";
+
+interface Props {
+    className?: string;
+}
+
+export const Container: React.FC<Props> = ({className, children}) => {
+    return (
+        <div className={cn('mx-auto max-w-[1280px]', className)}>{children}</div>
+    );
+};
