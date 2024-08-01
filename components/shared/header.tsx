952f2ee1cc3f351b305dedd860185fd10c9a2afb
Emilo067 <gasanovemil467@gmail.com>
2024-08-01 12:06:42 +0300

    Fix header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const Header: React.FC<Props> = ({className}) => {
     return (
-        <header className={cn('border border-b', className | undefined)}>
+        <header className={cn('border border-b', className)}>
             <Container className={'flex items-center justify-between py-8'}>
 
                 {/*{Левая часть}*/}
